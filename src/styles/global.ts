b6ad1444d5083b26e5c965ca5e98511a09196dff
Brspontes <brian.robert16@hotmail.com>
2021-03-18 19:55:42 -0300

    styled application

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   :root {
     --backgroud: #F0F2F5;
+    --green: #33CC95;
     --red: #E52E4D;
     --blue: #5429CC;
     --blue-light: #6933FF;
@@ -28,7 +29,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: var(---backgroud);
+    background: var(--backgroud);
     -webkit-font-smoothing: antialiased;
   }
 
