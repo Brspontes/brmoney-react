0d77da86d3f1c9b7eaa6679c96eaf53b7d4a856a
Brspontes <brian.robert16@hotmail.com>
2021-03-19 19:41:02 -0300

    initial implementation react-modal

--- src/components/Header/indext.tsx
+++ src/components/Header/indext.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
+import Modal from 'react-modal'
 
-export function Header () {
+interface HeaderProps {
+  onOpenNewReansactionModal: () => void
+}
+
+export function Header ({ onOpenNewReansactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="br money"/>
-        <button type="button">
+        <button type="button" onClick={onOpenNewReansactionModal}>
           Nova transação
         </button>
       </Content>
